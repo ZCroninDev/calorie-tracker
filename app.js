177475a15bbc850e79539f3221ff663ec8ff92d3
zach <ZCroninDev@users.noreply.github.com>
2021-11-07 14:18:06 -0500

    Add to calories WIP

--- app.js
+++ app.js
@@ -71,7 +71,8 @@ const UICtrl = (function() {
 		itemList: '#item-list',
     addBtn: '.add-btn',
 		itemNameInput: '#item-name',
-		itemCaloriesInput: '#item-calories'
+		itemCaloriesInput: '#item-calories',
+    totalCalories: '.total-calories'
 	}
 
   // Public methods  
@@ -121,6 +122,9 @@ const UICtrl = (function() {
     hideList: function(){
       document.querySelector(UISelectors.itemList).style.display = 'none';
     },
+    showTotalCalories: function(totalCalories){
+      document.querySelector(UISelectors.totalCalories).textContent = totalCalories;
+    },
     getSelectors: function(){
       return UISelectors;
     }
@@ -155,6 +159,9 @@ const App = (function(ItemCtrl, UICtrl) {
 
       // Get total calories
       const  totalCalories = ItemCtrl.getTotalCalories();
+      // Add total calories to UI
+      UICtrl.clearInput(totalCalories);
+
 
       // Clear fields 
       UICtrl.clearInput();
