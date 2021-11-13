8a19868dec8ae2067ebf22188d20e70de356cfa1
zach <ZCroninDev@users.noreply.github.com>
2021-11-13 17:00:04 -0500

    Delete and clear WIP

--- app.js
+++ app.js
@@ -71,6 +71,18 @@ const ItemCtrl = (function(){
       });
       return found;
     },
+    deleteItem: function(id){
+      // Get ids
+      const ids = data.items.map(function(item){
+        return item.id;
+      });
+
+      // Get index
+      const index = ids.indexOf(id);
+      
+      // Remove item
+      data.items.splice(index, 1);
+    },
     setCurrentItem: function(item){
       data.currentItem = item;
     },
@@ -229,6 +241,12 @@ const App = (function(ItemCtrl, UICtrl){
 
     // Update item event
     document.querySelector(UISelectors.updateBtn).addEventListener('click', itemUpdateSubmit);
+
+    // Delete item event
+    document.querySelector(UISelectors.deleteBtn).addEventListener('click', itemDeleteSubmit);
+    
+    // Back button event 
+    document.querySelector(UISelectors.backBtn).addEventListener('click', UICtrl.clearEditState);
   }
 
   // Add item submit
@@ -298,6 +316,17 @@ const App = (function(ItemCtrl, UICtrl){
      UICtrl.showTotalCalories(totalCalories);
 
      UICtrl.clearEditState();
+
+    e.preventDefault();
+  }
+
+  // Delete button event
+  const itemDeleteSubmit = function(e){
+    // Get current item
+    const currentItem = ItemCtrl.getCurrentItem();
+
+    // Delete from data structure
+    ItemCtrl.deleteItem(currentItem.id);
 
     e.preventDefault();
   }
